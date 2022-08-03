e4f167c82b2970b897fe5d4c46708a274f1dab61
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-08-03 05:36:09 +0530

    Updating terraform-loadbalancers

---
+++ env-prod/prod-backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-gvl"
+key    = "alb/prod/terraform.tfstate"
+region = "us-east-1"
---
+++ main.tf
@@ -0,0 +1,11 @@
+module "alb-public" {
+  source = "./vendor/modules/alb"
+  ALB_NAME = "roboshop-public"
+  INTERNAL = false
+}
+
+module "alb-private" {
+  source = "./vendor/modules/alb"
+  ALB_NAME = "roboshop-private"
+  INTERNAL = true
+}
