712f488896ceb6dcf91e5013eb67162fe1467c72
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-08-30 13:07:18 +0530

    Updating terraform-loadbalancers

--- vars.tf
+++ vars.tf
@@ -3,3 +3,9 @@ variable "ENV" {}
 variable "ALB_ARN" {}
 variable "LISTENER_ARN" {}
 variable "ALB_ADDRESS" {}
+
+variable "PUBLIC_ALB_ARN" {}
+variable "PRIVATE_ALB_ARN" {}
+variable "PRIVATE_LISTENER_ARN" {}
+variable "PRIVATE_ALB_ADDRESS" {}
+variable "PUBLIC_ALB_ADDRESS" {}
